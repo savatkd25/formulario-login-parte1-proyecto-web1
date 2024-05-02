e01c964e18c8c91854582b2a5f770654fe29287f
Edwin Dennis Rivadeneira Salvatierra <e1315400067@live.uleam.edu.ec>
2024-05-01 23:15:19 -0500

    Cambios de comentarios
    Co-Authored-By: Edwin Rivadeneira  <76056809+savatkd@users.noreply.github.com>

--- JS/registro.js
+++ JS/registro.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
         const email = document.getElementById('email').value
         const password = document.getElementById('password').value
 
-        // Aquí puedes hacer lo que quieras con los datos, como enviarlos a un servidor o mostrarlos en la consola
         console.log('Nombre:', firstName)
         console.log('Apellido:', lastName)
         console.log('Dirección:', address)
@@ -21,6 +20,5 @@ document.addEventListener('DOMContentLoaded', function () {
         console.log('Correo Electrónico:', email)
         console.log('Contraseña:', password)
 
-        // Aquí puedes agregar código para enviar los datos a un servidor usando AJAX, Fetch, etc.
     })
 })
